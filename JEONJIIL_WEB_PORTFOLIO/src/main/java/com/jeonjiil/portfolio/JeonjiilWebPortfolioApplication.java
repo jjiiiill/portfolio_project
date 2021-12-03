567a812079ec78290594ba785c98d0f407278637
jeonjiil <jeonjiil@allwon21.com>
2021-12-03 15:37:47 +0900

    init project

---
+++ JEONJIIL_WEB_PORTFOLIO/src/main/java/com/jeonjiil/portfolio/JeonjiilWebPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.jeonjiil.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeonjiilWebPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeonjiilWebPortfolioApplication.class, args);
+	}
+
+}
